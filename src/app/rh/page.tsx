6635e75e406d61d30d1601e830ca6632fe45b3fb
Tiodevs <Tiodevs>
2024-10-17 19:54:37 -0300

    feat(rh): Mostrando todos os users, redirecioando para perfil e signup

--- src/app/rh/page.tsx
+++ src/app/rh/page.tsx
@@ -1,26 +1,61 @@
-import { getCookiesServer } from "@/lib/cookieServer";
+'use client'
+
 import { Header } from "../components/header";
 import { Title } from "../components/title";
-import Image from "next/image";
+import Image from 'next/image'
+
+import { useRouter } from 'next/navigation'; // Importando o useRouter
 
 import styles from './styles.module.scss';
 import { api } from "../services/api";
+import { getCookiesClient } from "@/lib/cookieClient";
+import { useEffect, useState } from "react";
 
-export default async function Adm() {
+import { Eye, FileLock2, Trash2, UserRoundPen } from "lucide-react";
 
-    const token = getCookiesServer();
+export default function Adm() {
 
-    const users = await api.get('users', {
-        headers: {
-            Authorization: `Bearer ${token}`
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(false);
+    const [register, setRegister] = useState(false);
+
+    const router = useRouter(); // Inicializando o hook useRouter
+
+    useEffect(() => {
+
+        const token = getCookiesClient();
+
+        async function getUser() {
+            try {
+                const response = await api.get("/users", {
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                setUser(response.data);
+                setLoading(true)
+                console.log(response.data)
+            } catch (error) {
+                console.error("Erro ao carregar o usuário:", error);
+            }
         }
-    });
 
-    const resUsers = users.data;
+
+
+        getUser();
+    }, [register])
+
+    // Função para redirecionar para a página de signup
+    const handleAddNewUser = () => {
+        router.push('/rh/signup');
+    };
+
+    // Função para redirecionar para a página de profile
+    const handleViewProfile = (userId: string) => {
+        router.push(`/rh/profile/${userId}`);
+    };
+
 
     return (
         <div>
-
             <main className={styles.main}>
                 <Header />
                 <div className={styles.content}>
@@ -28,10 +63,43 @@ export default async function Adm() {
                         name01="Adicione, remova e edite informações"
                         name02="Toda a esquipe"
                     />
-                    <div>
-                        <p>Teste</p>
+                    <div className={styles.tabela}>
+
+                        <button onClick={handleAddNewUser}>
+                            Adicionar um novo
+                        </button>
+
+
+                        <div className={styles.tabelaheader}>
+                            <p className={styles.rowNome}>Nome</p>
+                            <p className={styles.rowTipo}>Tipo</p>
+                            <p className={styles.rowAtivo}>Status</p>
+                            <p className={styles.rowbtn}>Ações</p>
+                        </div>
+
+                        {loading ? (user && user.map((item: any) => <div className={styles.tabelarow}>
+                            <div className={styles.rowNome}>
+                                <Image
+                                    alt="Logo Sujeito Pizza"
+                                    src={item.profilePhoto}
+                                    width={45}
+                                    height={45}
+                                    priority={true}
+                                    quality={100}
+                                    className={styles.imgPerson}
+                                />
+                                <p>{item.name}</p>
+                            </div>
+                            <p className={styles.rowTipo}>{item.role}</p>
+                            <p className={styles.rowAtivo}>{item.active ? "Ativo" : "Dasativo"}</p>
+                            <div className={styles.rowbtn}>
+                                <Eye size={24} color="#FFFF" onClick={() => handleViewProfile(item.id)} />
+                                <UserRoundPen size={24} color="#FFFF" />
+                                <FileLock2 size={24} color="#FFFF" />
+                            </div>
+                        </div>)) : <></>}
+
                     </div>
-                    
                 </div>
             </main >
         </div >
